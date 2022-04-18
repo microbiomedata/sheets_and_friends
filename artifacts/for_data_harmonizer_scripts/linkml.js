7685f0cafcceb37e4f8aded7853fdb11b667954b
Brandon Davis <git@subdavis.com>
2022-04-18 10:20:17 -0400

    Update override linkml

--- artifacts/for_data_harmonizer_scripts/linkml.js
+++ artifacts/for_data_harmonizer_scripts/linkml.js
@@ -1,3 +1,112 @@
+/** --------------------------------------------------------- */
+/** Messaging interface for NMDC Portal-- Do not modify above */
+/** --------------------------------------------------------- */
+
+/**
+ * Call from the regular launch function to set up
+ * additional behavior for integration for NMDC submission portal
+ *
+ * This method is called when a new template is launched.
+ */
+
+DataHarmonizer.overrideGetColumnCoordinates = function () {
+    const ret = {};
+    let column_ptr = 0;
+    for (section of this.template) {
+        ret[section.title] = { '': column_ptr };
+        for (column of section.children) {
+            ret[section.title][column.title] = column_ptr;
+            column_ptr++;
+        }
+    }
+    return ret;
+};
+
+DataHarmonizer.updateParentState = function () {
+    const columnCoordinates = this.overrideGetColumnCoordinates();
+    const INVALID_CELLS = this.invalid_cells;
+    window.parent.postMessage({ type: 'update', INVALID_CELLS, columnCoordinates }, "*");
+    console.log('UpdateParentState', INVALID_CELLS, columnCoordinates);
+};
+
+/**
+ * Handle new file upload.
+ */
+DataHarmonizer.processFileChange = function (file) {
+    const ext = file.name.split('.').pop();
+    const acceptedExts = ['xlsx', 'xls', 'tsv', 'csv'];
+    if (!acceptedExts.includes(ext)) {
+        const errMsg = `Only ${acceptedExts.join(', ')} files are supported`;
+        $('#open-err-msg').text(errMsg);
+        $('#open-error-modal').modal('show');
+    } else {
+        this.invalid_cells = {};
+        this.runBehindLoadingScreen(this.openFile, [this, file]);
+        $('#file_name_display').text(file.name);
+    }
+    // Allow consecutive uploads of the same file
+    $('#open-file-input')[0].value = '';
+
+    $('#next-error-button,#no-error-button').hide();
+    this.current_selection = [null, null, null, null];
+}
+
+const setupMessageInterface = (dh) => {
+    const params = new URLSearchParams(location.search);
+    if (params.get('minified') || false) {
+        console.log("Setting up message interface");
+        $('#data-harmonizer-toolbar').css('display', 'none');
+
+        dh.updateParentState();
+
+        window.addEventListener("message", async (event) => {
+            console.log('child received event', event.data.type);
+            switch (event.data.type) {
+                case 'setupTemplate':
+                    dh.setupTemplate(event.data.folder);
+                    break;
+
+                case 'open':
+                    dh.processFileChange(event.data.files);
+                    break;
+
+                case 'validate':
+                    dh.validate();
+                    dh.updateParentState();
+                    break;
+
+                case 'jumpToRowCol':
+                    dh.scrollTo(event.data.row, event.data.column);
+                    break;
+
+                case 'changeVisibility':
+                    if (['all', 'required', 'recommended'].includes(event.data.value)) {
+                        dh.changeColVisibility(`show-${event.data.value}-cols-dropdown-item`);
+                    } else {
+                        const ptr = Object.keys(dh.overrideGetColumnCoordinates()).indexOf(event.data.value);
+                        dh.changeColVisibility(`show-section-${ptr}`);
+                    }
+                    break;
+
+                case 'exportJson':
+                    const value = [...dh.getFlatHeaders(), ...dh.getTrimmedData()];
+                    window.parent.postMessage({ type: 'exportJson', value }, "*");
+                    break;
+
+                case 'loadData':
+                    while (!dh.hot) {
+                        await new Promise((resolve) => window.setTimeout(resolve, 100));
+                    }
+                    dh.hot.loadData(event.data.data);
+                    break;
+
+                default:
+                    console.log('Unknown Type', event.data.type);
+            }
+        });
+    }
+}
+
 /************************** APPLICATION LAUNCH ********************/
 
 $(document).ready(async () => {
@@ -27,4 +136,5 @@ $(document).ready(async () => {
 	
 	await toolbar.refresh(); 
 
+    setupMessageInterface(dh);
 });
