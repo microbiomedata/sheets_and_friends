30f5ffac04101794df5e44aa0e2b1e005bf24426
Mark Andrew Miller <MAM@lbl.gov>
2022-08-24 16:56:48 -0400

    back to linkml.py

---
+++ docs/template/menu.js
@@ -0,0 +1,139 @@
+const TEMPLATES = {
+  "nmdc_submission_schema": {
+    "air": {
+      "name": "air",
+      "status": "published",
+      "display": true
+    },
+    "biofilm": {
+      "name": "biofilm",
+      "status": "published",
+      "display": true
+    },
+    "bioscales": {
+      "name": "bioscales",
+      "status": "published",
+      "display": true
+    },
+    "built_env": {
+      "name": "built_env",
+      "status": "published",
+      "display": true
+    },
+    "hcr-cores": {
+      "name": "hcr-cores",
+      "status": "published",
+      "display": false
+    },
+    "hcr-fluids-swabs": {
+      "name": "hcr-fluids-swabs",
+      "status": "published",
+      "display": false
+    },
+    "host-associated": {
+      "name": "host-associated",
+      "status": "published",
+      "display": true
+    },
+    "misc-envs": {
+      "name": "misc-envs",
+      "status": "published",
+      "display": false
+    },
+    "plant-associated": {
+      "name": "plant-associated",
+      "status": "published",
+      "display": true
+    },
+    "sediment": {
+      "name": "sediment",
+      "status": "published",
+      "display": true
+    },
+    "soil": {
+      "name": "soil",
+      "status": "published",
+      "display": true
+    },
+    "soil_emsl": {
+      "name": "soil_emsl",
+      "status": "published",
+      "display": true
+    },
+    "soil_emsl_jgi_mg": {
+      "name": "soil_emsl_jgi_mg",
+      "status": "published",
+      "display": true
+    },
+    "soil_emsl_jgi_mt": {
+      "name": "soil_emsl_jgi_mt",
+      "status": "published",
+      "display": true
+    },
+    "soil_jgi_mg": {
+      "name": "soil_jgi_mg",
+      "status": "published",
+      "display": true
+    },
+    "soil_jgi_mt": {
+      "name": "soil_jgi_mt",
+      "status": "published",
+      "display": true
+    },
+    "wastewater_sludge": {
+      "name": "wastewater_sludge",
+      "status": "published",
+      "display": true
+    },
+    "water": {
+      "name": "water",
+      "status": "published",
+      "display": true
+    },
+    "emsl_mixin": {
+      "name": "emsl_mixin",
+      "status": "published",
+      "display": false
+    },
+    "jgi_mg_mixin": {
+      "name": "jgi_mg_mixin",
+      "status": "published",
+      "display": false
+    },
+    "jgi_mt_mixin": {
+      "name": "jgi_mt_mixin",
+      "status": "published",
+      "display": false
+    },
+    "samp_id_new_terms_mixin": {
+      "name": "samp_id_new_terms_mixin",
+      "status": "published",
+      "display": false
+    },
+    "soil_mixs_inspired_mixin": {
+      "name": "soil_mixs_inspired_mixin",
+      "status": "published",
+      "display": false
+    },
+    "attribute value": {
+      "name": "attribute value",
+      "status": "published",
+      "display": false
+    },
+    "agent": {
+      "name": "agent",
+      "status": "published",
+      "display": false
+    },
+    "activity": {
+      "name": "activity",
+      "status": "published",
+      "display": false
+    },
+    "quantity value": {
+      "name": "quantity value",
+      "status": "published",
+      "display": false
+    }
+  }
+}
